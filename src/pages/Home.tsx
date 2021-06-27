6b1a623a60f81489a7b7316c45c6d7e2a54fc9c1
walopes <walopes23@gmail.com>
2021-06-27 16:43:19 -0300

    Added style for the Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,30 +3,35 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import '../styles/auth.scss';
+
 export function Home(){
     return (
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="Ilustração simbolizando símbolos especiais" />
                 <strong>Crie salas de Q&amp;A ao-vivo</strong>
                 <p>Tire suas dúvidas em tempo-real</p>
             </aside>
             <main>
-                <img src={logoImg} alt="letmeask" />
-                <button>
-                    <img src={googleIconImg} alt="logo do google"/>
-                    Crie sua conta com o Google
-                </button>
-                <div>Ou entre em uma sala</div>
+                <div className="main-content">
+                    <img src={logoImg} alt="letmeask" />
+                    <button className="create-room">
+                        <img src={googleIconImg} alt="logo do google"/>
+                        Crie sua conta com o Google
+                    </button>
+                    <div className="separator">Ou entre em uma sala</div>
 
-                <form>
-                    <input
-                        type="text"
-                        placeholder="Digite o código da sala" 
-                    />
-                    <button type="submit">Entrar na sala</button>
-                </form>
-
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Digite o código da sala" 
+                        />
+                        <button type="submit">
+                            Entrar na sala
+                        </button>
+                    </form>
+                </div>
             </main>
         </div>
     )
