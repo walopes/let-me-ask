2da575ac53a9515d1dc21315238de550e3aa006c
walopes <walopes23@gmail.com>
2021-06-27 21:30:45 -0300

    Added new page "NewRoom"

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
-    <Home />
+    <NewRoom />
   );
 }
 
