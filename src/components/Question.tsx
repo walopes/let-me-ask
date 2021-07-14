2942046552201d246b4afa47fc0de349eeda85f9
walopes <walopes23@gmail.com>
2021-07-14 07:53:38 -0300

    Added like button on Question's at Room page

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,6 @@
 
+import { Children, ReactNode } from 'react';
+
 import '../styles/question.scss';
 
 type QuestionProps = {
@@ -6,10 +8,16 @@ type QuestionProps = {
     author: {
         name: string;
         avatar: string;
-    }
+    };
+    children?: ReactNode;
 };
 
-export function Question({content, author}: QuestionProps){
+export function Question(
+    {
+        content, 
+        author,
+        children,
+    }: QuestionProps) {
     return(
         <div className="question">
             <p>{content}</p>
@@ -18,7 +26,7 @@ export function Question({content, author}: QuestionProps){
                     <img src={author.avatar} alt={author.name} />
                     <span>{author.name}</span>
                 </div>
-                <div></div>
+                <div>{children}</div>
             </footer>
         </div>
     )
