599cf413e711526b66c092775fb276aff4697e90
walopes <walopes23@gmail.com>
2021-07-16 21:35:05 -0300

    QuestionType changed hasLiked boolean field to likeId string field, due to logic of only one like per user

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -28,7 +28,7 @@ type QuestionType = {
     isAnswered: boolean;
     isHighlighted: boolean;
     likeCount: number;
-    hasLiked: boolean;
+    likeId: string | undefined;
 };
 
 
@@ -61,7 +61,7 @@ export function useRoom(roomId: string){
                     isHighlighted: value.isHighlighted,
                     isAnswered: value.isAnswered,
                     likeCount: Object.values(value.likes ?? {}).length,
-                    hasLiked: Object.values(value.likes ?? {}).some(like => like.authorId === user?.id),
+                    likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
                 }
             });
 
